6c2c40c653967be4fbb1399fe3521ff7f8fe39ba
Dominika <dominika275@gmail.com>
2019-03-18 14:22:22 +0100

    lab2-finished tasks 1-4

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,20 +1,20 @@
 from matplotlib.pyplot import *
 from numpy import *
 
-#TASKS (4p)
-#1 calculate & print the value of function y = 2x^2 + 2x + 2 for x=[56, 57, ... 100] (0.5p)
-#2 ask the user for a number and print its factorial (1p)
-#3 write a function which takes an array of numbers as an input and finds the lowest value. Return the index of that element and its value (1p)
-#4 looking at lab1-input and lab1-plot files create your own python script that takes a number and returns any chart of a given length.
-#the length of a chart is the input to your script. The output is a plot (it doesn't matter if it's a y=x or y=e^x+2x or y=|x| function, use your imagination)
-#test your solution properly. Look how it behaves given different input values. (1p)
-#5 upload the solution as a Github repository. I suggest creating a directory for the whole python course and subdirectories lab1, lab2 etc. (0.5p)
+# TASKS (4p)
+# 1 calculate & print the value of function y = 2x^2 + 2x + 2 for x=[56, 57, ... 100] (0.5p)
+# 2 ask the user for a number and print its factorial (1p)
+# 3 write a function which takes an array of numbers as an input and finds the lowest value. Return the index of that element and its value (1p)
+# 4 looking at lab1-input and lab1-plot files create your own python script that takes a number and returns any chart of a given length.
+# the length of a chart is the input to your script. The output is a plot (it doesn't matter if it's a y=x or y=e^x+2x or y=|x| function, use your imagination)
+# test your solution properly. Look how it behaves given different input values. (1p)
+# 5 upload the solution as a Github repository. I suggest creating a directory for the whole python course and subdirectories lab1, lab2 etc. (0.5p)
 
-'''
+
 # TASK 1
 
 def equation(x):    
-    return 2 * x
+    return 2**2 + 2 * x + 2
 
 
 for i in range(56, 101):
@@ -29,21 +29,20 @@ x = int(input('Insert your value here which ia a natural number:'))
 
 def factorial(x):
 
-    if x in range(0,2):
+    if x in range(0, 2):
         return 1
-    elif x>2:
-        return x*factorial(x-1)
+    elif x < 0:
+        return 'Insert other value'
     else:
-        return 'Error'
-
+        return x*factorial(x-1)
 
 print(factorial(x))
 
 print('-'*20)
-'''
+
 # TASK 3
 
-myArr = [8, 4, 3, 6, 2, 54, 2, 2]
+myArr = [2, 4, 3, 6, 2, 54, 2, 2]
 minNr = myArr[0]
 minIndexPosition = []
 
@@ -52,26 +51,25 @@ for i in range(0, len(myArr)):
     if myArr[i] < minNr:
         minNr = myArr[i]
 
-
 for i in range(0, len(myArr)):
 
     if myArr[i] == minNr:
         minIndexPosition.append(i)
-
 
 if len(minIndexPosition) == 1:
     print('There is one minimum found: number', minNr)
 else:
     print('There are found', len(minIndexPosition), 'minimums on positions', minIndexPosition, 'in the array')
 
-
-print('-'*20)
+print('-' * 20)
 
 # TASK 4
 
-a = int(input(f"Choose the length of chart:"))
+def chart(a):
+    x = linspace(-5, a, 5)
+    y = 2 * x ** 2
+    plot(x, y)
+    show()
 
-x = linspace(0, a, 50)
-y = 2*x**2
-plot(x, y)
-show()
+
+chart(5)
--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,67 +1,105 @@
+from cs50 import get_int
+import math
+from decimal import Decimal
+
 # TASKS (8p)- calculate & print:
-#0 Use alternative way of reading inputs - using library (0.5p)
-#1 Perimeter & field of circles with given radius X for the first circle & Y for the second one. (1p)
-#2 Find X & Y that satisfy: X is divisible by Y and both X & Y are even. (0.5p)
-#3 Check if X is divisible by Y (do it in one line of code), print 'X is divisible by Y' or 'X is not divisible by Y'. (1p)
-#Ad 3 Hint- use the "ternary operator" as we did calculating xIsEvenLog above.
-#4 Add rounding for the above x/y operation. Round to 2 decimal points. Hint: look up in Google "python limiting number of decimals". (1p)
-#5 Look at lab2-plot.py and create your own script which takes a number as an input and plots the same 3D wave but with different characteristics
+
+# 0 Use alternative way of reading inputs - using library (0.5p)
+# 1 Perimeter & field of circles with given radius X for the first circle & Y for the second one. (1p)
+# 2 Find X & Y that satisfy: X is divisible by Y and both X & Y are even. (0.5p)
+# 3 Check if X is divisible by Y (do it in one line of code),
+#   print 'X is divisible by Y' or 'X is not divisible by Y'. (1p)
+# Ad 3 Hint- use the "ternary operator" as we did calculating xIsEvenLog above.
+# 4 Add rounding for the above x/y operation. Round to 2 decimal points.
+#   Hint: look up in Google "python limiting number of decimals". (1p)
+# 5 Look at lab2-plot.py and create your own script which takes a number
+#   as an input and plots the same 3D wave but with different characteristics
 # it's totally up to your imagination how do you want to amend the figure according to the input number (1p)
-#6 Test your code. Check various edge cases. In other words: does your program (1, 3, 4 & 5)work for all input values?
+# 6 Test your code. Check various edge cases. In other words: does your program (1, 3, 4 & 5)work for all input values?
 # In case of task 4  do not forget to round to different amount of decimals and see if it still works.(3p)
 
-'''
-#TASK 0
 
-from cs50 import get_int
+# TASK 0
+
 x = get_int("x: ")
 y = get_int("y: ")
 
 print('-'*20)
 
-#TASK 1
+# TASK 1
 
-import math
+# perimeterOfCircle1 = 2*math.pi * x
+# perimeterOfCircle2 = 2*math.pi * y
 
+# fieldOfCircle1 = math.pi*math.pow(x, 2)
+# fieldOfCircle2 = math.pi*math.pow(y, 2)
 
-
-perimeterOfCircle1=2*math.pi*x
-perimeterOfCircle2=2*math.pi*y
-
-fieldOfCirlce1=math.pi*math.pow(x,2)
-fieldOfCirlce1=math.pi*math.pow(y,2)
-
-print(perimeterOfCircle1, fieldOfCirlce1)
-
+# print(perimeterOfCircle1, fieldOfCircle1)
 
 
 class Circle:
-    def __init__(self, R):
-        self.R = R
+    def __init__(self, radius):
+        self.radius = radius
 
     def perimeter_circle(self):
-        print(2 * math.pi * self.R)
+        if self.radius > 0:
+            print('Perimeter od this circle equals:', 2 * math.pi * self.radius)
+        elif self.radius < 0:
+            print('Radius can\'t be negative ')
 
     def field_circle(self):
-        print(math.pi*math.pow(self.R, 2))
+        if self.radius > 0:
+            print('Field od this circle equals:', math.pi*math.pow(self.radius, 2))
+        elif self.radius < 0:
+            print('Radius can\'t be negative ')
 
 
 circle1 = Circle(x)
 circle2 = Circle(y)
 
+print('CIRCLE 1')
 circle1.perimeter_circle()
+circle1.field_circle()
 
-'''
+print('CIRCLE 2')
+circle2.perimeter_circle()
+circle2.field_circle()
+
 print('-' * 20)
 
 
-#TASK 2
+# TASK 2
 
-#TASK 3
+if x % y == 0 and x % 2 == 0 and y % 2 == 0:
+    print('These numbers sadisfy requirements')
+else:
+    print('Find other numbers')
 
-#TASK 4
 
-#TASK 5
+print('-'*20)
+
+# TASK 3
+
+isDivisibleByY = x % y == 0
+print('X is divisible by Y') if isDivisibleByY else print('X is not divisible by Y')
+
+
+print('-'*20)
+
+# TASK 4
+
+convertToDecimal = Decimal(x/y)
+
+output = round(convertToDecimal, 2)
+print(output)
+
+
+print('-'*20)
+
+
+'''
+# TASK 5
+'''
 
 
 
