0f5627b56e4cde3660417e3eddb04f1f494b7a67
Dominika <dominika275@gmail.com>
2019-04-02 17:52:30 +0200

    completed lab3

--- lab3/lab3-2.py
+++ lab3/lab3-2.py
@@ -1,21 +1,23 @@
 import math
 from decimal import Decimal
+
+
 # 1 Write a function countField which calculates the field of a given figure. It takes the following input parameters:
 # - type: circle/rectangle/triangle/rhombus
 # - x & optional y.
 # For circle we get only x which stands for radius. For Rectangle x&y are the figure's sides, for triangle they are
 # accordingly the base and the height and for rhombus - diagonals (4p)
 
-# 2 Write a function which takes sets of parameters of two figures and compares their fields. (4p)
+# 2 Write a function which takes sets of parameters of two figures and compares their Figure. (4p)
 # The exemplary input is [[Circle, 4], [Rhombus, 2, 4]]
 # Expected output would be 'The first figure (Circle) has larger field'
 # 3 Test your solutions
 
-# TASK 2
+# TASK 1 & 2
 
 
-class Fields:
-    def __init__(self, figure_type, x, y=0):
+class Figure:
+    def __init__(self, figure_type=0, x=0, y=0):
         self.x = x
         self.y = y
         self.figure_type = figure_type
@@ -34,30 +36,59 @@ class Fields:
         elif self.figure_type == 'rhombus' and self.x > 0 and self.y > 0:
             result = 0.5 * self.x * self.y
             return result
+        elif self.x <= 0 or self.y <= 0:
+            print('You wrote negative values, your input should be positive')
+            return -1
         else:
-            result = 'Write another values'
-            return result
+            return 'Incorrect input'
 
     def to_string(self):
         return self.figure_type
 
 
-circle = Fields('circle', 1)
-rectangle = Fields('rectangle', 1, 2)
-triangle = Fields('triangle', 3, 2)
-rhombus = Fields('rhombus', 1, 2)
+def compare(figure1=0, figure2=0):
+    try:
+        if figure1.count_field() > figure2.count_field():
+            return str(figure1.to_string()) + ' has larger field'
+        elif figure1.count_field() < figure2.count_field():
+            return str(figure2.to_string()) + ' has larger field'
+        else:
+            return 'Fields are equal'
+    except TypeError as e:
+        print(e)
+
+    except AttributeError:
+        print("Object has no attributes!")
 
 
-def compare(figure1, figure2):
-    if figure1.count_field() > figure2.count_field():
-        return str(figure1.to_string()) + ' has larger field'
-    elif figure1.count_field() < figure2.count_field():
-        return str(figure2.to_string()) + ' has larger field'
-    else:
-        return 'Fields are equal'
+def main():
+
+    circle = Figure('ciRcle'.lower(), 1)
+    rectangle = Figure('recTangle'.lower(), 2.5, 3)
+    triangle = Figure('triAngle'.lower(), 3, 2)
+    rhombus = Figure('Rhombus'.lower(), 1, 2)
+
+    fieldOfCircle = circle.count_field()
+    fieldOfRectangle = rectangle.count_field()
+    fieldOfTriangle = triangle.count_field()
+    fieldOfRhombus = rhombus.count_field()
+
+    text = 'Calculated field of given '
+    text2 = ' is: '
+
+    print(text, circle.to_string(), text2, fieldOfCircle)
+    print(text, rectangle.to_string(), text2, fieldOfRectangle)
+    print(text, triangle.to_string(), text2, fieldOfTriangle)
+    print(text, rhombus.to_string(), text2, fieldOfRhombus)
+
+    print('-' * 20)
+    try:
+        print(compare(circle, circle))
+        print(compare(triangle, rectangle))
+    except TypeError as e:
+        print(e)
+    print('-' * 20)
 
 
-print(compare(circle, rectangle))
-print(compare(triangle, rectangle))
-
-print('-'*20)
+if __name__ == '__main__':
+    main()
--- lab3/lab3.py
+++ lab3/lab3.py
@@ -13,8 +13,8 @@ from decimal import Decimal
 
 # TASK 1
 
-
-def count_field(figure_type, x, y=0):
+'''
+def count_field(figure_type = 0, x = 0, y=0):
     text = 'Calculated field of given '
     text2 = ' is '
     if figure_type == 'circle' and x > 0:
@@ -48,5 +48,63 @@ print(circle)
 print(rectangle)
 print(triangle)
 print(rhombus)
+
+print('-'*20)
+'''
+
+
+def count_field(figure_type=0, x=0, y=0):
+
+    if figure_type == 'circle' and x > 0:
+        convertToDecimal = Decimal(math.pi * x ** 2)
+        result = round(convertToDecimal, 2)
+        return result
+    elif figure_type == 'rectangle' and x > 0 and y > 0:
+        result = x * y
+        return result
+    elif figure_type == 'triangle' and x > 0 and y > 0:
+        result = 0.5 * x * y
+        return result
+    elif figure_type == 'rhombus' and x > 0 and y > 0:
+        result = 0.5 * x * y
+        return result
+    else:
+        result = 'Write another values'
+        return result
+
+
+circle = count_field('circle', 1)
+rectangle = count_field('rectangle', 1, 5)
+triangle = count_field('triangle', 3, 2)
+rhombus = count_field('rhombus', 1, 2)
+
+
+text = 'Calculated field of given '
+text2 = ' is '
+circleText = 'circle'
+rectangleText = 'rectangle'
+triangleText = 'triangle'
+rhombusText = 'rhombus'
+
+print(text, circleText, text2, circle)
+print(text, rectangleText, text2, rectangle)
+print(text, triangleText, text2, triangle)
+print(text, rhombusText, text2, rhombus)
+
+print('-'*20)
+
+
+def compare(figure1=0, figure2=0):
+
+    if figure1 > figure2:
+        return figure1
+    elif figure1 < figure2:
+        return figure2
+    else:
+        return 'Fields are equal'
+
+
+print(compare(circle, rhombus))
+print(compare(triangle, rectangle))
 
 print('-'*20)
