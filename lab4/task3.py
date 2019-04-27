c98c9c67750ca024766457feca662e3432c9944b
Dominika <dominika275@gmail.com>
2019-04-27 14:36:24 +0200

    list4-finished task 3

--- lab4/task3.py
+++ lab4/task3.py
@@ -1,7 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+import math
+
 # TASKS (9p)
 
 # 3 Find a differential equation which represents a process / model (your choice)
 # and implement it using odeint python function (1p)
 
 
-# TASK 3
+# model: Function name that returns derivative values
+# at requested y and t values as dydt = model(y,t)
+
+
+def differentialEquation(y0, tk):
+    # function that returns dy/dt
+    def model(y, t):
+        # dydt = -y + 1.0
+        dydt = 3.0 * np.exp(-t)
+        return dydt
+
+
+    # time points
+    t = np.linspace(0, tk)
+
+    y = odeint(model, y0, t)
+
+    print('Funkcja, której stanem początkowym jest y0 =', y0, ' , a czas końcowy równa się tk =', tk)
+
+    plt.plot(t, y)
+    plt.xlabel('time')
+    plt.ylabel('y(t)')
+    plt.show()
+
+
+try:
+    differentialEquation(0, 5)
+
+except TypeError as e:
+    print(e)
+
+except AttributeError as e:
+    print(e)
--- lab4/task4.py
+++ lab4/task4.py
@@ -1,6 +1,6 @@
 # TASKS (9p)
 
-# 4 Look at the example of optimization for exponential function.
+# 4 Look at the example of optimization for exponential function. wykladnicza
 # Did you encounter any errors? Where in code do we display the optimal point?
 # Do we minimize or maximize and which function?
 # Start your search always from the point (0, -2). (1p)
