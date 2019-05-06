c2d8d7bd41eefd1257ea3f581c5d03342a0582ee
Dominika <dominika275@gmail.com>
2019-05-06 23:01:02 +0200

    starting task 5

--- lab4/task5.py
+++ lab4/task5.py
@@ -1,11 +1,14 @@
 import numpy as np
+from numpy import exp,arange
+from pylab import meshgrid,cm,imshow,contour,clabel,colorbar,axis,title,show
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
-from scipy import linspace , cos , exp, random, meshgrid, zeros
+from scipy import linspace , cos , exp, random, meshgrid, zeros, optimize
 from scipy.optimize import fmin
 from matplotlib.pyplot import plot, show, legend, figure, cm, contour, clabel
 from mpl_toolkits.mplot3d import Axes3D
-
+from scipy.optimize import basinhopping
+import time
 # TASKS (9p)
 
 # 5 Create your own 3d function with multiple local optima.
@@ -15,32 +18,14 @@ from mpl_toolkits.mplot3d import Axes3D
 # You can, for example, execute your search function multiple times and check
 # if the returned result is what you expected.
 # Measure the success / total trials rate (2p).
-'''
+
 
 # TASK 5
-fig = plt.figure()
-ax = fig.gca(projection='3d')
 
-# Make data.
-X = np.arange(-5, 5, 0.25)
-xlen = len(X)
-Y = np.arange(-5, 5, 0.25)
-ylen = len(Y)
-X, Y = np.meshgrid(X, Y)
-R = np.sqrt(X**2 + Y**2)
-Z = np.sin(R)
-
-surf = ax.plot_surface(X, Y, Z,  linewidth=0)
-
-# Customize the z axis.
-ax.set_zlim(-1, 1)
-ax.w_zaxis.set_major_locator(LinearLocator(6))
-
-plt.show()
+'''
 
 def f(x):
-    # return exp(-x[0] ** 2 - x[1] ** 2)
-    return np.sin(np.sqrt(x[0] ** 2 + x[1] ** 2))
+    return 5*(1-(x[0]**2+x[1]**3))*np.e**(-(x[0]**2+x[1]**2)/2)
 
 
 def neg_f(x):
@@ -65,12 +50,18 @@ ax.plot([x_min[0]], [x_min[1]], [f(x_min)], color='k', marker='o', markersize=5,
 ax.legend()
 show()
 
+x0=[5.]
+minimizer_kwargs = {"method": "BFGS"}
+ret = basinhopping(f, x0, minimizer_kwargs=minimizer_kwargs, niter=200)
+print("global minimum: x = %.4f, f(x0) = %.4f" % (ret.x, ret.fun))
+
+
+
 '''
-
+start = time.time()
 def f(x):
-    # return exp(-x[0] ** 2 - x[1] ** 2)
-
-    return np.sin(np.sqrt(x[0] ** 2 + x[1] ** 2))
+    return 5*(1-(x[0]**2+x[1]**3))*np.e**(-(x[0]**2+x[1]**2)/2)
+    #return np.cos(14.5 * x - 0.3) + (x + 0.2) * x
 
 
 def neg_f(x):
@@ -79,7 +70,7 @@ def neg_f(x):
 x0 = random.randn(2)
 x_min = fmin(neg_f, x0)
 
-delta = 5
+delta = 3
 x_knots = linspace(x_min[0] - delta, x_min[0] + delta, 41)
 y_knots = linspace(x_min[1] - delta, x_min[1] + delta, 41)
 X, Y = meshgrid(x_knots, y_knots)
@@ -88,9 +79,13 @@ for i in range(Z.shape[0]):
     for j in range(Z.shape[1]):
         Z[i][j] = f([X[i, j], Y[i, j]])
 
+
 ax = Axes3D(figure(figsize=(8, 5)))
 ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm, linewidth=0.4)
 ax.plot([x0[0]], [x0[1]], [f(x0)], color='g', marker='o', markersize=5, label='initial')
 ax.plot([x_min[0]], [x_min[1]], [f(x_min)], color='k', marker='o', markersize=5, label='final')
 ax.legend()
 show()
+
+end = time.time()
+print(end - start)
