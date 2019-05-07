78f153548735fe1cf801c4d8c0e48eb5bdbbb109
Dominika <dominika275@gmail.com>
2019-05-07 18:50:09 +0200

    finished task 5

--- lab4/task5.py
+++ lab4/task5.py
@@ -1,14 +1,10 @@
 import numpy as np
-from numpy import exp,arange
-from pylab import meshgrid,cm,imshow,contour,clabel,colorbar,axis,title,show
-import matplotlib.pyplot as plt
-from scipy.integrate import odeint
 from scipy import linspace , cos , exp, random, meshgrid, zeros, optimize
 from scipy.optimize import fmin
 from matplotlib.pyplot import plot, show, legend, figure, cm, contour, clabel
 from mpl_toolkits.mplot3d import Axes3D
-from scipy.optimize import basinhopping
 import time
+
 # TASKS (9p)
 
 # 5 Create your own 3d function with multiple local optima.
@@ -22,17 +18,11 @@ import time
 
 # TASK 5
 
-'''
-
 def f(x):
     return 5*(1-(x[0]**2+x[1]**3))*np.e**(-(x[0]**2+x[1]**2)/2)
 
-
-def neg_f(x):
-    return -f(x)
-
 x0 = random.randn(2)
-x_min = fmin(neg_f, x0)
+x_min = fmin(f, x0)
 
 delta = 3
 x_knots = linspace(x_min[0] - delta, x_min[0] + delta, 41)
@@ -43,49 +33,34 @@ for i in range(Z.shape[0]):
     for j in range(Z.shape[1]):
         Z[i][j] = f([X[i, j], Y[i, j]])
 
-ax = Axes3D(figure(figsize=(8, 5)))
-ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm, linewidth=0.4)
-ax.plot([x0[0]], [x0[1]], [f(x0)], color='g', marker='o', markersize=5, label='initial')
-ax.plot([x_min[0]], [x_min[1]], [f(x_min)], color='k', marker='o', markersize=5, label='final')
-ax.legend()
-show()
 
-x0=[5.]
-minimizer_kwargs = {"method": "BFGS"}
-ret = basinhopping(f, x0, minimizer_kwargs=minimizer_kwargs, niter=200)
-print("global minimum: x = %.4f, f(x0) = %.4f" % (ret.x, ret.fun))
-
-
-
-'''
 start = time.time()
-def f(x):
-    return 5*(1-(x[0]**2+x[1]**3))*np.e**(-(x[0]**2+x[1]**2)/2)
-    #return np.cos(14.5 * x - 0.3) + (x + 0.2) * x
+for i in range(200):
+    x1 = 20*random.rand(2) - 10
+    x1_min = fmin(f, x1)
+    print(x1)
+    end = time.time()
+    duration = end - start
+    if (f(x1_min) > f(x_min) and duration < 0.30):
+        global_min = x1_min
+    else:
+        break
+        print(x1_min)
 
 
-def neg_f(x):
-    return -f(x)
-
-x0 = random.randn(2)
-x_min = fmin(neg_f, x0)
-
-delta = 3
-x_knots = linspace(x_min[0] - delta, x_min[0] + delta, 41)
-y_knots = linspace(x_min[1] - delta, x_min[1] + delta, 41)
-X, Y = meshgrid(x_knots, y_knots)
-Z = zeros(X.shape)
-for i in range(Z.shape[0]):
-    for j in range(Z.shape[1]):
-        Z[i][j] = f([X[i, j], Y[i, j]])
-
+print('Czas szukania globalnego', duration)
 
 ax = Axes3D(figure(figsize=(8, 5)))
 ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm, linewidth=0.4)
 ax.plot([x0[0]], [x0[1]], [f(x0)], color='g', marker='o', markersize=5, label='initial')
-ax.plot([x_min[0]], [x_min[1]], [f(x_min)], color='k', marker='o', markersize=5, label='final')
+ax.plot([x_min[0]], [x_min[1]], [f(x_min)], color='k', marker='o', markersize=5, label='lokal min')
+# ax.plot([lokal_optimum[0]], [lokal_optimum[1]], [f(lokal_optimum)], color='k', marker='o', markersize=5, label='lokal otimum')
+
+ax.plot([x1[0]], [x1[1]], [f(x1)], color='m', marker='o', markersize=5, label='initial')
+ax.plot([x1_min[0]], [x1_min[1]], [f(x1_min)], color='red', marker='o', markersize=5, label='global min')
+# ax.plot([global_optimum[0]], [global_optimum[1]], [f(global_optimum)], color='red', marker='o', markersize=5, label='global otimum')
 ax.legend()
 show()
 
-end = time.time()
-print(end - start)
+# end = time.time()
+# print(end - start)
