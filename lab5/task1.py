4a2eb4acd44d5e5328f10dafc1296f750cb8aa90
Dominika <dominika275@gmail.com>
2019-05-12 19:22:07 +0200

    starting list 5

---
+++ lab5/task1.py
@@ -0,0 +1,6 @@
+#TASKS (11p)
+# To use the requests library you have to install it first. If you have pip and you're using python3 interpreter in your project
+# then simply pip3 install requests
+
+# 1 Find another public API with cryptocurrency and compare prices. As an output print:
+# "Currently the XXX exchange market is better for buying whilst YYY is better for selling" (3p)
